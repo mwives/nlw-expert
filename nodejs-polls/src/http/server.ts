5552b1eccfa14f159f7107ff1cf28c2caa9ea506
Ives M. W <ivesmw@gmail.com>
2024-02-07 11:56:34 -0300

    feat: create route for get poll

--- nodejs-polls/src/http/server.ts
+++ nodejs-polls/src/http/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
 
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 const app = fastify()
 const PORT = 3333
 
-app.register(createPoll)
+app.register(createPoll, getPoll)
 
 app.listen({ port: PORT }).then(() => {
   console.log(`Server is running on port ${PORT}`)
