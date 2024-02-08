e8234429cfe3f0734ccf87b67e76b1352dcb9e16
Ives M. W <ivesmw@gmail.com>
2024-02-08 20:20:33 -0300

    chore: add websocket

--- nodejs-polls/src/http/server.ts
+++ nodejs-polls/src/http/server.ts
@@ -1,9 +1,11 @@
 import fastifyCookie from '@fastify/cookie'
+import fastifyWebsocket from '@fastify/websocket'
 import fastify from 'fastify'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 const PORT = 3333
@@ -12,10 +14,12 @@ app.register(fastifyCookie, {
   secret: 'polls-app-nlw',
   hook: 'onRequest',
 })
+app.register(fastifyWebsocket)
 
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: PORT }).then(() => {
   console.log(`Server is running on port ${PORT}`)
